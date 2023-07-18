19815fb85ef2cceeea32e525bbf50087208aab46
Boodenator <boode1000@gmail.com>
2023-07-18 13:16:30 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include "main.h"
 	/**
-	 * main - Entry point
-	 * Description: 'prints _ putchar to stdout'
-	 * Return: always 0
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
 	 */
-int main(void)
-{
+
+
+	int main(void)
+	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
