8df96d042b15b5dfb8395f6bfefd80a7ac480ff5
Boodenator <boode1000@gmail.com>
2023-08-24 16:57:14 -0700

    print-list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -24,7 +24,7 @@ int _strlen(char *s)
  */
 size_t print_list(const list_t *h)
 {
-	size_t j = 0;
+	size_t i = 0;
 
 	while (h)
 	{
@@ -33,5 +33,5 @@ size_t print_list(const list_t *h)
 		j++;
 	}
 
-	return (j);
+	return (i);
 }
