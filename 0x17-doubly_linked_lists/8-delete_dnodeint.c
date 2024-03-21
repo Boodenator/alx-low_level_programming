9f21adfc36ab98dbd4b2b49daa727a3c1834febe
Boodenator <boode1000@gmail.com>
2024-03-21 15:35:14 -0700

    task8 new fx

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes node at index of dlistint_t list
  * @head: head of the list
  * @index: index of the new node
  * Return: 1 if  succeess, -1 if failure
