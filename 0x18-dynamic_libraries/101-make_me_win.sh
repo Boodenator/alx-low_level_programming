4a5c39880f393081c06b87e558e049656a18d97a
Boodenator <boode1000@gmail.com>
2024-03-25 17:23:50 -0700

    tsk3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Boodenator/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp/ https://github.com/Boodenator/alx-low_level_programming/blob/1e927acfabe5666ac18c3b3fde689005d5bccc50/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
