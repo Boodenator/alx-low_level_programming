3f509cc042ad76132de523b70ecefb83e3221c98
Boodenator <boode1000@gmail.com>
2024-03-25 17:16:32 -0700

    task3 upd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Boodenator/alx-low_level_programming/blob/e589fa7d87ee44d1f5aedde91da7e0453e0ac9fc/0x18-dynamic_libraries/mylib.so
+wget -P /tmp/ https://github.com/Boodenator/alx-low_level_programming/master/0x18-dynamic_libraries/mylib.so
 export LD_PRELOAD=/tmp/mylib.so
